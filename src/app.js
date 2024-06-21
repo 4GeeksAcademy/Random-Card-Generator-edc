e65354df57dabb76ac148232e9f8f9aa9c0f2795
emanueldelc <165728564+emanueldelc@users.noreply.github.com>
2024-06-21 01:04:49 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,8 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  const myArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  const randomValue = myArray[Math.floor(Math.random() * myArray.length)];
+  document.querySelector(".btn").addEventListener("click", function() {
+    generateCard();
+  });
 };
+function generateCard() {
+  const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let suits = ["♦", "♣", "♥", "♠"];
+  let rankIndex = Math.floor(Math.random() * ranks.length);
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+
+  if (suits[suitsIndex] == "♦" || suits[suitsIndex] == "♥") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+  } else {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+  }
+  document.querySelector(".top-suit").innerHTML = suits[suitsIndex];
+  document.querySelector(".bottom-suit").innerHTML = suits[suitsIndex];
+  document.querySelector(".number").innerHTML = ranks[rankIndex];
+}
